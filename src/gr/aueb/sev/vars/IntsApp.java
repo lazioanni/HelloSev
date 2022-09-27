3e4333f9903ce7f31a6a3dab7cb179a08010995a
lazioanni <lazioanni@gmail.com>
2022-09-27 18:34:02 +0300

    Initial Commit

---
+++ src/gr/aueb/sev/vars/IntsApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.sev.vars;
+
+public class IntsApp {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum;
+
+        sum = num1 + num2;
+
+        System.out.println("The sum is "+sum);
+    }
+}
